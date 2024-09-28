8d1951ebb19b1bc59c56eabdcdd4057b79dd5485
jipenalver <jianchris2k14@gmail.com>
2024-09-28 22:25:30 +0800

    feat: added slot and validators and login form

--- src/main.js
+++ src/main.js
@@ -3,15 +3,15 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import App from './App.vue'
+import router from './router'
+
 // Vuetify
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-
-import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
 
--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,32 +1,44 @@
 <script setup>
-import { ref } from 'vue'
+import { useDisplay } from 'vuetify'
+import AppLayout from '@/components/auth/layout/AppLayout.vue'
+import LoginForm from '@/components/auth/LoginForm.vue'
 
-const theme = ref('light')
-
-function onClick() {
-  theme.value = theme.value === 'light' ? 'dark' : 'light'
-}
+const { mobile } = useDisplay()
 </script>
 
 <template>
-  <v-responsive class="border rounded">
-    <v-app :theme="theme">
-      <v-app-bar class="px-3">
-        <v-spacer></v-spacer>
+  <AppLayout>
+    <template #content>
+      <v-row>
+        <v-col cols="12" md="7"> </v-col>
+        <v-col cols="12" md="5">
+          <v-card class="mx-auto" variant="tonal">
+            <template #title>
+              <div class="text-center">
+                <h1 class="font-weight-black">DBM | HRMS</h1>
 
-        <v-btn
-          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-          text="Toggle Theme"
-          slim
-          @click="onClick"
-        ></v-btn>
-      </v-app-bar>
+                <v-divider />
 
-      <v-main>
-        <v-container>
-          <h1>Main Content</h1>
-        </v-container>
-      </v-main>
-    </v-app>
-  </v-responsive>
+                <h2>Login</h2>
+              </div>
+            </template>
+
+            <v-card-text class="pt-4">
+              <v-img
+                class="mx-auto mb-5"
+                :width="mobile ? '80%' : '300'"
+                aspect-ratio="16/9"
+                cover
+                src="/images/logo-dbm.png"
+              />
+
+              <v-divider />
+
+              <LoginForm />
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </template>
+  </AppLayout>
 </template>
